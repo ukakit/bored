b502ae064cb3ed7ea4a3b0626ecb8cecc1dd9855
kenny <ukakit@hotmail.com>
2022-12-05 00:24:39 -0800

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <>
+        <ul className="navbar">
+            <a href="/">Channel 1</a>
+            <a href='/'>Channel 2</a>
+        </ul>
+    </>
+  )
+}
