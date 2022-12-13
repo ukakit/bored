0711f9f7025a21da4fd7ed0e59a0e1f5a6f78643
kenny <ukakit@hotmail.com>
2022-12-12 23:37:11 -0800

    change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ export const NavBar = () => {
         <ul className="navbar">
             <a href="/">Channel 1</a>
             <a href='/'>Channel 2</a>
+            <a href='/'>Channel 3</a>
         </ul>
     </>
   )
